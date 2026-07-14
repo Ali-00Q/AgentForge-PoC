958256918b545e581b3da56bfe198a1297363b69
Abdelrahman844 <Bodyakram7@gmail.com>
2026-07-14 03:44:57 +0300

    Added requirements agent and prompt

--- model/gemini.py
+++ model/gemini.py
@@ -1,5 +1,17 @@
 from dotenv import load_dotenv
+from google import genai
 import os
 
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+class GeminiModel:
+    _client = genai.Client(
+        api_key = os.getenv("GEMINI_API_KEY")
+    )
+    _model = "gemini-2.5-flash"
+    @classmethod
+    def generate(cls, prompt: str) -> str:
+        response = cls._client.models.generate_content(
+            model=cls._model,
+            contents=prompt,
+        )
+        return response.text.strip()
